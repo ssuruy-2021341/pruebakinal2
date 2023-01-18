3282661ed068ad6301efc423d755de748bcea6e8
Sergio Suruy <ssuruy-2021341@kinal.edu.gt>
2023-01-18 14:52:15 -0600

    Primer commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Mi primer repositorio en internet 
+
+
+Saludos a todos gente 
---
+++ index.js
@@ -0,0 +1,7 @@
+function sumar() {
+    let numero1 = document.getElementById("valor1").value;
+    let numero2 = document.getElementById("valor2").value;
+
+    let Suma = perseInt(numero1) + parseInt(numero2);
+    console.log(sumar);
+}
